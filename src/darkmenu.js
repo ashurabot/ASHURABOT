04bd86ce27b61e1730277d82ea490f24d5a88fa0
ashurabot <77414400+ashurabot@users.noreply.github.com>
2021-01-13 23:37:41 -0300

    Add files via upload

---
+++ src/darkmenu.js
@@ -0,0 +1,41 @@
+const darkmenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *𝘾𝙊𝙈𝘼𝙉𝘿𝙊𝙎 𝘿𝙊 𝘼𝙎𝙃𝙐𝙍𝘼:*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+╔════════════════════
+  TRADUZIDO POR *DARK*
+  DUVIDAS? 👇
+  WA.me/5527992752045
+╚════════════════════`
+}
+
+exports.ashuramenu = ashuramenu
+
+
+
+
+
+
+
+
+
